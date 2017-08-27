d812c6e087614d7f171c18f6f9aab3c45fe46638
Eric LEGBA <eleongithub@gmail.com>
2017-08-27 16:10:19 +0200

    Correction Sonar

--- apps-web-service/src/main/java/com/syscom/apps/web/security/AuthenticationTokenProvider.java
+++ apps-web-service/src/main/java/com/syscom/apps/web/security/AuthenticationTokenProvider.java
@@ -3,6 +3,7 @@ package com.syscom.apps.web.security;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -11,10 +12,10 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
+
 import com.syscom.apps.business.service.TokenService;
 import com.syscom.apps.dto.TokenDTO;
 import com.syscom.apps.dto.referentiel.FunctionDTO;
-import com.syscom.apps.model.Token;
 import com.syscom.apps.web.utils.Functions;
 
 @Component
@@ -26,22 +27,6 @@ public class AuthenticationTokenProvider implements AuthenticationProvider{
 	@Override
 	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
 		String authorization = authentication.getName();
-//		Token token = tokenService.findValidToken(authorization);
-//		if(token == null){
-//			throw new BadCredentialsException("Unvalid token. Unauthorized access.");
-//		}
-//		
-//		List<Function> functions = new ArrayList<>();
-//		if( token.getCustomer() != null && token.getCustomer().getRole() != null){
-//			functions  = token.getCustomer().getRole().getFunctions(); 
-//		}		
-//		
-//		List<GrantedAuthority> grantedAuthorities = functions.stream().map(function -> new SimpleGrantedAuthority(Functions.ROLE+function.getCode()))
-//	                                                                            .collect(Collectors.toList());
-//	        
-//		JwtAuthenticationToken jwtAuthenticationToken = new JwtAuthenticationToken(authorization, grantedAuthorities);
-//		jwtAuthenticationToken.setAuthenticated(true);
-//		return jwtAuthenticationToken;
 		
 		TokenDTO tokenDTO = tokenService.findToken(authorization);
 		if(tokenDTO == null){
